124ac6f131e3234182c9ab8c429fa84969ac85c4
Pranav <lifeispranav@gmail.com>
2025-06-04 00:17:17 +0530

    Feat: Added Controller fn for video uploadation

---
+++ src/controllers/video.controllers.js
@@ -0,0 +1,18 @@
+const { uploadVideo } = require("../repository/videos.repository")
+const { VideoSchemaValidator } = require("../validators/video.validators")
+
+
+const uploadVideoCtr = async (req, res) => {
+
+  const { data, error } = VideoSchemaValidator.safeParse(req.body)
+  if(error) throw error
+
+  const video = await uploadVideo(req.email, data)
+  console.log("Video Uploaded Successfully!")
+
+  res.json({
+    msg: "Video Uploaded Succesfully, Now you can watch it!"
+  })
+
+}
+
