73a6574ecce8c90fdc7e01b5cff6f87eed946d9f
Pranav <lifeispranav@gmail.com>
2025-06-04 01:27:40 +0530

    Feat: Added Routing setup for our YT, Fix: Fixed the alignment/sequence of routers in video.routers.js

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,17 +1,27 @@
 const express = require('express')
 
-const { uploadVideoCtr, getCreatorCtr, watchVideo, allVideoCtr } = require('../controllers/video.controllers')
+const { uploadVideoCtr, getCreatorCtr, watchVideo, allVideoCtr, deleteVideoCtr, getMyVideosCtr, getVideosByCategoryCtr, getVideosByTagCtr } = require('../controllers/video.controllers')
 const authMiddleware = require('../middlewares/auth.middleware')
 const { errorMiddleware } = require('../error/errormiddleware')
 
 
 const videoRouter = express.Router()
 
-videoRouter.use(authMiddleware)
-videoRouter.post('/upload', uploadVideoCtr)
 videoRouter.get('/all', allVideoCtr)
+videoRouter.get('/categories/:category', getVideosByCategoryCtr)
 videoRouter.get('/watch/:videoUrl', watchVideo)
-videoRouter.get('/:videoUrl', getCreatorCtr)
+videoRouter.get('/creator/:videoUrl', getCreatorCtr)
+videoRouter.get('/tags/:tag', getVideosByTagCtr)
+
+
+videoRouter.use(authMiddleware)
+
+videoRouter.get('/myVideos', getMyVideosCtr)
+videoRouter.post('/upload', uploadVideoCtr)
+videoRouter.put('/update/:videoURL', uploadVideoCtr)
+videoRouter.delete('/delete', deleteVideoCtr)
+
 videoRouter.use(errorMiddleware)
+
 
 module.exports = videoRouter
