920993685703a53ff5cbded3171ca2f0e97a526a
Pranav <lifeispranav@gmail.com>
2025-06-04 00:03:40 +0530

    Added upload video, and getCreator Fns

--- src/repository/videos.repository.js
+++ src/repository/videos.repository.js
@@ -1,7 +1,22 @@
+const { Users, Videos } = require("../schemas");
 
 
 const uploadVideo = async (userMail, videoDet) => {
 
+  const { videoUrl, thumbnail_url, category, tags } = videoDet
+  const user = Users.findOne({userMail})
+
+  const newVideo = new Videos({
+    userId: user.userId,
+    videoUrl,
+    thumbnail_url,
+    category,
+    tags
+  })
+
+  await newVideo.save()
+
+  return newVideo
 }
 
 const deleteVideo = async (userMail, videoUrl) => {
@@ -10,6 +25,14 @@ const deleteVideo = async (userMail, videoUrl) => {
 
 const updateVideo = async (userMail, videoUrl, updatedDet) => {
 
+}
+
+const getCreator = async (videoUrl) => {
+
+  const video = await Videos.findOne({videoUrl})
+  const user = await Users.findById(video.userId)
+
+  return user.channelName
 }
 
 
